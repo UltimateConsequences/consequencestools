9515a896b0fc0cf44b2ef12b8a32f6a9b5832836
CarwilB <54286746+CarwilB@users.noreply.github.com>
2024-08-24 07:44:51 -0700

    Move future scripts to staging folder

--- R/here_filename.R
+++
@@ -1,7 +0,0 @@
-here_filename <- function(filename="directory/name.ext"){
-  path <- dirname(filename)
-  filename_base <- basename(filename)
-  filename_full <- here::here(path, filename_base)
-  
-  filename_full
-}
