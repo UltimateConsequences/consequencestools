e41df7bd68845a7ef0b700a73d0e9b4db76c035b
CarwilB <54286746+CarwilB@users.noreply.github.com>
2024-08-26 12:05:43 -0700

    update imports

--- data-raw/deaths-entries-loader.R
+++ data-raw/deaths-entries-loader.R
@@ -1,4 +1,4 @@
-de.filename <- "raw-data/deaths-entries-2024-08-12.rds"
+de.filename <- "data-raw/deaths-entries-2024-08-12.rds"
 
 deaths_aug24 <- readr::read_rds(here_filename(de.filename))
 usethis::use_data(deaths_aug24)
