40bfc40b32f1c07e772a771640c1e562dd389759
CarwilB <54286746+CarwilB@users.noreply.github.com>
2024-08-28 18:55:37 -0700

    clean_dates(): a function used on raw spreadsheet data.

---
+++ R/clean-dates.R
@@ -0,0 +1,65 @@
+globalVariables(c("later_date_flag"))
+# clean_dates(),
+# A function that processes multiple options:
+# 1.It treats blanks as "unknown", but only if there is in fact a date.
+# 2.It treats the first date in the text field as numerical date
+# 3.It saves the whole non-numerical field as a note in a relevant variable.
+
+clean_dates <- function(de_chardates){
+  de_chardates <- de_chardates %>%
+    mutate(day_notes = case_when(is.na(day) ~ "Unknown day",
+                                 !str_detect(day,"^([0-9]*)$") ~ day,
+                                 TRUE ~ ""
+    ))
+  de_chardates <- de_chardates %>%
+    mutate(month_notes = case_when(is.na(month) ~ "Unknown month",
+                                   !str_detect(month,"^([0-9]*)$") ~ month,
+                                   TRUE ~ ""
+    ))
+  de_chardates <- de_chardates %>%
+    mutate(year_notes = case_when(is.na(year) ~ "Unknown year",
+                                  !str_detect(year,"^([0-9]*)$") ~ year,
+                                  TRUE ~ ""
+    ))
+
+  de_chardates <- de_chardates %>%
+    mutate(later_date_flag = !(is.na(later_day) & is.na(later_month) & is.na(later_year))) # this flag is true if there is later date
+
+  # Diagnostic variable lets us see if that worked.
+  #
+  #  de_test <- de_chardates %>%
+  #    dplyr::select(later_year, later_month, later_day, later_date_flag)
+
+  de_chardates <- de_chardates %>%
+    mutate(later_day_notes = case_when(!later_date_flag  ~ "", # return nothing if all three columns are blank
+                                       is.na(later_day) ~ "Unknown day",
+                                       !str_detect(later_day,"^([0-9]*)$") ~ day,
+                                       TRUE ~ ""
+    ))
+  de_chardates <- de_chardates %>%
+    mutate(later_month_notes = case_when(!later_date_flag ~ "", # return nothing if all three columns are blank
+                                         is.na(later_month) ~ "Unknown month",
+                                         !str_detect(later_month,"^([0-9]*)$") ~ month,
+                                         TRUE ~ ""
+    ))
+  de_chardates <- de_chardates %>%
+    mutate(later_year_notes = case_when(!later_date_flag ~ "", # return nothing if all three columns are blank
+                                        is.na(later_year) ~ "Unknown year",
+                                        !str_detect(later_year,"^([0-9]*)$") ~ year,
+                                        TRUE ~ ""
+    ))
+  de_chardates <- de_chardates %>% dplyr::select(-"later_date_flag") # Remove temporary flag
+
+  date_num_variables <- c("year", "month", "day", "later_year", "later_month", "later_day")
+
+  de_chardates <- de_chardates %>%
+  #  mutate_at(date_num_variables, str_extract, "\\d+")
+    mutate(dplyr::across(date_num_variables, ~ str_extract(.x, pattern = "\\d+")))
+
+  de_chardates <- de_chardates %>%
+    # mutate_at(date_num_variables, as.integer)
+    mutate(dplyr::across(date_num_variables, ~ as.integer(.x, pattern = "\\d+")))
+  #    mutate(across(.cols = year:later_day, .fns = as.integer) )
+
+  return(de_chardates)
+}
---
+++ tests/testthat/_snaps/clean-dates.md
@@ -0,0 +1,27 @@
+# clean_dates(): Notes are functional
+
+    Code
+      date_notes
+    Output
+      # A tibble: 18 x 4
+         day_notes      month_notes     year_notes         n
+         <chr>          <chr>           <chr>          <int>
+       1 Unknown day    "Unknown month" "Unknown year"    36
+       2 Unknown day    "Unknown month" ""                24
+       3 Unknown day    "Unknown month" ""                24
+       4 Unknown day    "Unknown month" ""                24
+       5 19 or 20 or 21 ""              ""                14
+       6 30 or unknown  ""              ""                13
+       7 Unknown day    ""              ""                11
+       8 Unknown day    ""              ""                11
+       9 Unknown day    ""              ""                11
+      10 Unknown day    ""              ""                11
+      11 Unknown day    ""              ""                11
+      12 Unknown day    ""              ""                11
+      13 Unknown day    ""              ""                11
+      14 Unknown day    ""              ""                11
+      15 Unknown day    ""              ""                11
+      16 Unknown day    ""              ""                11
+      17 5 or 6         ""              ""                 6
+      18 5 or 6         ""              ""                 6
+
