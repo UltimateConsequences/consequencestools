b314909a199477076db69dd4eaaf31c8f02e5f6f
CarwilB <54286746+CarwilB@users.noreply.github.com>
2024-09-18 11:33:01 -0700

    New function processes the event_date_range_table ot find the number of events onoging in a day or month.

---
+++ R/event-range-tools.R
@@ -0,0 +1,45 @@
+#' Count Ongoing Events on a Specific Date
+#'
+#' @param input_date A Date object specifying the date to check for ongoing events
+#'
+#' @return An integer representing the number of events ongoing on the input date
+#'
+#' @examples
+#' event_range_table <- event_date_range_table(deaths_aug24)
+#' num_events <- event_range_table %>% count_ongoing_events(as.Date("1988-06-27"))
+#' print(paste("Number of ongoing events:", num_events))
+#'
+#' @export
+count_ongoing_events <- function(event_date_range_table, input_date) {
+  ongoing_events <- event_date_range_table %>%
+    filter(date_first <= input_date & date_last >= input_date)
+
+  return(nrow(ongoing_events))
+}
+
+
+#' Count Events Overlapping with a Specific Month
+#'
+#' @param yearmonth A yearmon object specifying the month to check for overlapping events
+#'
+#' @return An integer representing the number of events overlapping with the specified month
+#'
+#' @examples
+#' num_events <- count_events_in_month(as.yearmon("Jan 2003"))
+#' print(paste("Number of overlapping events:", num_events))
+#'
+#' @import zoo
+#' @export
+count_events_in_month <- function(event_date_range_table, yearmonth) {
+  require(zoo)
+
+  # Convert yearmonth to start and end dates of the month
+  start_date <- as.Date(yearmonth)
+  end_date <- as.Date(yearmonth) + months(1) - days(1)
+
+  # Filter events overlapping with the month
+  overlapping_events <- event_date_range_table %>%
+    filter(date_first <= end_date & date_last >= start_date)
+
+  return(nrow(overlapping_events))
+}
