dee30929c12404f94043173839d0e0436a1e4941
CarwilB <54286746+CarwilB@users.noreply.github.com>
2024-12-11 14:40:53 -0800

    deal with atomic vector issue

--- R/naming-functions.R
+++ R/naming-functions.R
@@ -207,7 +207,7 @@ add_detailed_label <- function(dataframe){
   }
 
   dataframe <- dataframe %>% rowwise() %>% mutate(
-    protest_domain_color = protest_domain$colors[protest_domain],
+    protest_domain_color = protest_domain$colors[[protest_domain]],
     styled_protest_domain =
       str_glue("<span style=\"color:{protest_domain_color};\"><b>",
                "{protest_domain}</b></span>")
